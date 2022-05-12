4979c9f84e5f42c9cf402d82fabdff18812b5bec
anhlh-3049 <le.hoang.anh@sun-asterisk.com>
2022-05-12 08:30:03 +0700

    static_page_design

--- db/migrate/20220510092434_create_rooms.rb
+++ db/migrate/20220510092434_create_rooms.rb
@@ -4,7 +4,7 @@ class CreateRooms < ActiveRecord::Migration[6.1]
       t.string :name
       t.decimal :price
       t.text :description
-      t.integer :status, default:0
+      t.integer :status, default: 0
       t.references :user, null: false, foreign_key: true
       t.references :room_view, null: false, foreign_key: true
 
